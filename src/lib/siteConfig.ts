d2fab1da9855c5fc3b760a05b73f51a468b7b21b
ponk1-tech <ponk1.tech@gmail.com>
2026-01-31 17:04:33 +0900

    update

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   siteName: "Ponk1 Tech",
-  description: "日々の暮らしをちょっと便利にするアプリを、ひとつずつ丁寧に作っています。",
+  description: "日々の暮らしをちょっと便利にするアプリを開発しています。",
   noteUrl: "https://note.com/ponk1_tech",
   contactEmail: "ponk1.tech@gmail.com",
 } as const;
